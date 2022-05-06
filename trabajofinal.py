d4e36c0b530aaf6b7143f54d66fcc6e8ae01f8aa
lauralardies <lrodrlar@myuax.com>
2022-05-06 09:35:34 +0200

    analizamos csv

---
+++ trabajofinal.py
@@ -0,0 +1,27 @@
+import pyspark
+
+try:
+    f = open("air_traffic_data.csv")
+
+except FileNotFoundError:
+    print("El fichero no existe.")
+
+else:
+    lineas = f.readlines()
+    f.close()
+
+    linea = lineas[0].strip()
+    columnas = linea.split(",")
+        
+    datos = []
+    seleccion = ["Activity Period", "Operating Airline", "Operating Airline IATA Code", "Published Airline", "Published Airline IATA Code", "GEO Summary", "GEO Region", "Activity Type Code", "Price Category Code", "Terminal", "Boarding Area", "Passenger Count", "Adjusted Activity Type Code", "Adjusted Passenger Count", "Year", "Month"]
+
+    for linea in lineas[1:]:
+        dato = {}
+        linea = linea.strip()
+        campos = linea.split(",")
+        for i in range(len(columnas)):
+            if columnas[i] in seleccion:
+                dato[columnas[i]] = campos[i]
+        datos.append(dato)
+
